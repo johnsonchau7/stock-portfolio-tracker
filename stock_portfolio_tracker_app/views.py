6cf3da9f21c22e82d270e14c9845b87b6743761e
JCBEST <jcbest@192-168-1-106.tpgi.com.au>
2021-02-09 10:56:29 +1100

    added urls

--- stock_portfolio_tracker_app/views.py
+++ stock_portfolio_tracker_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    return render(request, 'login/login.html')
+
+def register(request):
+    return render(request, 'registration/register.html')
+
+def stockpage(request):
+    return render(request, 'stockpage/stockpage.html')
